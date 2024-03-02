251c924cbf397e204899fc79708d2f44e52548dc
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-02 17:39:10 +0530

    performed crud operation

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const http=require('http');
-http.createServer((req,res)=>{
-    res.write("<h1>Hello,welcome to my first server</h1>");
-    res.end();
-}).listen(4500);
+const fs=require('fs');
+const path=require('path');
+const dirPath=path.join(__dirname,'crud');
+const filepath=`${dirPath}/apple.txt`;
+
+//fs.writeFileSync(filepath,"This is simple text file");
+
+// fs.readFile(filepath,'utf8',(err,item)=>{
+//     console.log(item);
+// })
+
+//   fs.appendFile(filepath," and i am back",(err)=>{
+//     if(!err) console.log("File is updated")
+//   })
+
+//   fs.rename(filepath,`${dirPath}/fruit.txt`,(err)=>{
+//     if(!err) console.log("File is updated")
+//   })
+
+ fs.unlinkSync(`${dirPath}/fruit.txt`);

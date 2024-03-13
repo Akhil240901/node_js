99519af38dffc52e201fe8e76ca4c4529d69ba39
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-13 22:48:20 +0530

    remove url extention

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express=require('express');
 const app=express();
 
 const pubPath=path.join(__dirname,'public');
-app.use(express.static(pubPath));
-
+//app.use(express.static(pubPath));
+app.get('',(req,resp)=>{
+    resp.sendFile(`${pubPath}/index.html`)
+})
+app.get('about',(req,resp)=>{
+    resp.sendFile(`&{pubPath}/about.html`)
+})
 app.listen(5000);
 

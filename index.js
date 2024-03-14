29085426bad634ac93d4b12e449720b81df289ae
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-14 17:46:00 +0530

    template engine and remove url

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const app=express();
 
 const pubPath=path.join(__dirname,'public');
 //app.use(express.static(pubPath));
+app.set('view engine','ejs')
 app.get('',(req,resp)=>{
     resp.sendFile(`${pubPath}/index.html`)
 })
-app.get('about',(req,resp)=>{
-    resp.sendFile(`&{pubPath}/about.html`)
+app.get('/about',(req,resp)=>{
+    resp.sendFile(`${pubPath}/about.html`)
+})
+
+app.get('/profile',(req,resp)=>{
+    const user={
+        name:"akhil bisen",
+        city:"gondia",
+        email:"bisenakhl465@fmail.com"
+    }
+    resp.render('profile',{user})
 })
 app.listen(5000);
 

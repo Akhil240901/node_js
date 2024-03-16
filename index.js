6be1b03d203970b8b8eb507c52daed6ee67e3cd1
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-16 07:51:05 +0530

    performed read operation using node js on mongodb

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const {MongoClient}=require('mongodb');
-const url='mongodb://localhost:27017';
-const client=new MongoClient(url);
-const database='akhil_store';
+const mongoConnect=require('./mongoDB.js');
 
-async function getDat(){
-    const result=await client.connect();
-    const db=result.db(database);
-    const collection=db.collection('playlists');
-    const response=await collection.find({}).toArray();
-    console.log(response);
+async function main(){
+    const result=await mongoConnect();
+    //console.log(result); //will show all document
+    const data=await result.find({"name":SQL}).toArray();
+    console.log(data);
 }
 
-getDat();
+main();

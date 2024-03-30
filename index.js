21186ff5b29376ea7b084e1a9f2dc02e53936829
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-30 14:03:49 +0530

    search api with multile filled

--- index.js
+++ index.js
@@ -5,31 +5,14 @@ const Product = require("./products");
 const app = express();
 app.use(express.json());
 
-app.post("/create", async (req, resp) => {
-  let data = new Product(req.body);
-  let result = await data.save();
-  console.log(result);
-  resp.send(result);
-});
-
-app.get("/list/:_id", async (req, resp) => {
-  let data = await Product.find(req.params);
-  console.log(data);
-  resp.send(data);
-});
-
-app.put("/update/:_id", async (req, resp) => {
-  let data = await Product.updateOne(req.params,
-    {
-       $set: req.body,
-    }
-  );
-  resp.send(data);
-});
-
-app.delete("/delete/:_id",async(req,resp)=>{
-    let data=await Product.deleteOne(req.params);
+app.get("/read/:key",async(req,resp)=>{
+    let data=await Product.find({
+          "$or":[
+                {"bName":{$regex:req.params.key}},
+                {"country":{$regex:req.params.key}}
+          ]
+    }) 
+    console.log(req.params.key);
     resp.send(data);
-    console.log(data);
 })
 app.listen(5000);
